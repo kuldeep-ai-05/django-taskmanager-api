ded2e10377a6d5fde0d9bf4c89b251a1756a6dbf
Kuldeep Mohanty <kuldeep.mca2024@gmail.com>
2025-07-27 22:51:00 +0530

    Initial Commit - Django CRUD API Project

---
+++ tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TaskListCreate,TaskRetrieveUpdateDelete
+\
+urlpatterns = [
+    path('tasks/',TaskListCreate.as_view(),name='task-list-create'),
+    path('tasks/<int:pk>/',TaskRetrieveUpdateDelete.as_view(),name='task-detail'),
+]
