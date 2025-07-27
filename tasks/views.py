36d89ff2f138a010f372c5b7ad30a0b2caed8702
Kuldeep Mohanty <kuldeep.mca2024@gmail.com>
2025-07-27 23:27:18 +0530

    Added Filtering and Pagination with updated models

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TaskListCreate(generics.ListCreateAPIView):
     queryset=Task.objects.all()
     serializer_class=TaskSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['completed','priority']
 
 class TaskRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset=Task.objects.all()
