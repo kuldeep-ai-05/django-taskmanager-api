fa0ef48eb6dca4445a17f478e877285828cbd559
Kuldeep Mohanty <kuldeep.mca2024@gmail.com>
2025-07-28 00:54:13 +0530

    Added JWT Authentication

--- tasks/views.py
+++ tasks/views.py
@@ -2,12 +2,14 @@ from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 
 class TaskListCreate(generics.ListCreateAPIView):
     queryset=Task.objects.all()
     serializer_class=TaskSerializer
     filter_backends=[DjangoFilterBackend]
     filterset_fields=['completed','priority']
+    permission_classes=[IsAuthenticated]
 
 class TaskRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset=Task.objects.all()
